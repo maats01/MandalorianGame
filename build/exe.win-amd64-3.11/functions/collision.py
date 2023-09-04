fcd869ff80caa2e902ed52eaf4091467c00b8fb0
Matheus Sampaio <108765220+maats01@users.noreply.github.com>
2023-09-04 20:07:45 -0300

    Delete build/exe.win-amd64-3.11 directory

--- build/exe.win-amd64-3.11/functions/collision.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-def collision_check(group1, group2):
-    """Checagem de colisão entre qualquer elemento 
-    de dois grupos de sprites."""
-    return pygame.sprite.groupcollide(group1, group2, True, True, pygame.sprite.collide_mask)
--- build/exe.win-amd64-3.11/sprite_groups.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-player_sprites = pygame.sprite.GroupSingle()
-enemy_sprites = pygame.sprite.Group()
-bullet_sprites = pygame.sprite.Group()
-enemy_bullet_sprites = pygame.sprite.Group()
-bg_sprite = pygame.sprite.Group()
-
